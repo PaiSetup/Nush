06dab968ccc6ebe7463f56c6bce4016951bdb60d
MaciejDziuban <dziuban.maciej@gmail.com>
2020-01-16 02:28:21 +0100

    Add set_icons.sh

---
+++ BashUtils/Misc/set_icons.sh
@@ -0,0 +1,4 @@
+function set_icons() (
+    $ICON_SETTER_PATH -l D:\\Pictures\\Icons -d D: -d D:\\Pictures -d D:\\Video -d E:
+    $ICON_SETTER_PATH -r -d E:\\Programs
+)
--- BashUtils/paths.sh
+++ BashUtils/paths.sh
@@ -1,6 +1,7 @@
 export SCRIPTS_PATH=`realpath "$BASH_SOURCE" | xargs dirname`
 export NPP_PATH="E:\Programs\Notepad++\notepad++.exe"
 export VERACRYPT_PATH="E:\Programs\VeraCrypt\VeraCrypt-x64.exe"
+export ICON_SETTER_PATH="/d/Projects/IconSetter/build/Debug/IconSetter.exe"
 
 if [ "$1" == "debug" ]; then
     function print_path() (
@@ -10,5 +11,6 @@ if [ "$1" == "debug" ]; then
     print_path SCRIPTS_PATH
     print_path NPP_PATH
     print_path VERACRYPT_PATH
+    print_path ICON_SETTER_PATH
     echo "-------------------- paths.sh"
 fi
