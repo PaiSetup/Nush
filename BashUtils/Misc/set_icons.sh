74653541288a897e87bcf317c86883e19c215486
MaciejDziuban <dziuban.maciej@gmail.com>
2020-03-20 13:37:52 +0100

    Update iconfigure paths

--- BashUtils/Misc/set_icons.sh
+++ BashUtils/Misc/set_icons.sh
@@ -1,4 +1,4 @@
 function set_icons() (
-    $ICON_SETTER_PATH -l D:\\Pictures\\Icons -d D: -d D:\\Pictures -d D:\\Video -d E:
-    $ICON_SETTER_PATH -r -d E:\\Programs
+    $ICONFIGURE_PATH -l D:\\Pictures\\Icons -d D: -d D:\\Pictures -d D:\\Video -d E:
+    $ICONFIGURE_PATH -r -d E:\\Programs
 )
--- BashUtils/paths.sh
+++ BashUtils/paths.sh
@@ -1,16 +1,16 @@
 export SCRIPTS_PATH=`realpath "$BASH_SOURCE" | xargs dirname`
 export NPP_PATH="/e/Programs/Notepad++/notepad++.exe"
 export VERACRYPT_PATH="/e/Programs/VeraCrypt/VeraCrypt-x64.exe"
-export ICON_SETTER_PATH="/d/Projects/IconSetter/build/Debug/IconSetter.exe"
+export ICONFIGURE_PATH="/e/Programs/Iconfigure/Iconfigure.exe"
 
 if [ "$1" == "debug" ]; then
     function print_path() (
         if [ -e "${!1}" ]; then
-            msg=
+            msg="FOUND"
         else
-            msg="NOT FOUND!!!"
+            msg="NOT FOUND"
         fi
-        printf '%-16s %s %s\n' "$1" "${!1}" "$msg"
+        printf '%-16s %-40s %s\n' "$1" "${!1}" "$msg"
     )
     echo "-------------------- paths.sh"
     print_path SCRIPTS_PATH
