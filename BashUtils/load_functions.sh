14fd1121055fc55c1fccc8be3df57364d0219761
MaciejDziuban <dziuban.maciej@gmail.com>
2021-11-17 21:19:48 +0100

    Partially cleanup load_functions (as bash, not posix sh)

--- BashUtils/load_functions.sh
+++ BashUtils/load_functions.sh
@@ -1,13 +1,18 @@
-function load_functions() {
-    # Always execute in scripts directory
-    local scripts_path=`realpath "$BASH_SOURCE" | xargs dirname`
-    pushd "$scripts_path" > /dev/null
+#!/bin/bash
 
-    function generate_cached_paths_windows() {
+load_functions() {
+    # Temporarily go to the scripts directory
+    local scripts_path
+    scripts_path=$(realpath "${BASH_SOURCE[0]}" | xargs dirname)
+    initial_dir=$(pwd)
+    cd "$scripts_path" || return
+
+    generate_cached_paths_windows() {
         # Find programs directory
         for value in "/e/Programs" "/d/Programs" "/e/Programy" "/d/Programy"; do
             if [ -d "$value" ];  then
-                local programs_dir="$value"
+                local programs_dir
+                programs_dir="$value"
                 break
             fi
         done
@@ -18,17 +23,19 @@ function load_functions() {
         echo "Will be looking for programs in $programs_dir"
 
         # Helper function for finding executables and putting them into paths.sh
-        function find_exe() {
+        find_exe() {
             local variable_name="$1"
             local possible_directory_names="$2"
             local exe_name="$3"
-            for possible_directory_name in "$possible_directory_names"; do
-                local dir="$programs_dir/$possible_directory_name"
-                if [ ! -d "$dir" ]; then
+            for possible_directory_name in $possible_directory_names; do
+                local dir
+                dir="$programs_dir/$possible_directory_name"
+                if ! [ -d "$dir" ]; then
                     continue
                 fi
 
-                local found_path=`find $programs_dir/$possible_directory_name -type f -executable -name $exe_name | head -1`
+                local found_path
+                found_path=$(find "$programs_dir/$possible_directory_name" -type f -executable -name "$exe_name" | head -1)
                 if [ -n "$found_path" ]; then
                     add_entry "$variable_name" "$found_path"
                     return
@@ -43,12 +50,16 @@ function load_functions() {
         find_exe  ICONFIGURE_PATH Iconfigure     Iconfigure.exe
     }
 
-    function generate_cached_paths_linux() {
+    generate_cached_paths_linux() {
         # Helper function for finding binaries and putting them into paths.sh
-        function find_binary() {
-            local variable_name="$1"
-            local binary_name="$2"
-            local found_path=`which $binary_name`
+        find_binary() {
+            local variable_name
+            local binary_name
+            local found_path
+
+            variable_name="$1"
+            binary_name="$2"
+            found_path=$(which "$binary_name")
             if [ $? == 0 ]; then
                 add_entry "$variable_name" "$found_path"
             else
@@ -60,8 +71,8 @@ function load_functions() {
         find_binary XRANDR_PATH xrandr
     }
 
-    function generate_cached_paths() (
-        function add_entry() {
+    generate_cached_paths() (
+        add_entry() {
             local variable_name="$1"
             local variable_value="$2"
             loaded_paths+=("$variable_name")
@@ -71,10 +82,10 @@ function load_functions() {
         # Preamble
         local loaded_paths=()
         echo "# Paths loaded by load_functions.sh script, do not change it." > paths.sh
-        add_entry SCRIPTS_PATH $scripts_path
+        add_entry SCRIPTS_PATH "$scripts_path"
 
         # Os-specific search
-        local isLinux=`uname -a | grep Linux | wc -l`
+        local isLinux=$(uname -a | grep -c Linux)
         if [ $isLinux == 1 ]; then
             generate_cached_paths_linux
         else
@@ -82,7 +93,7 @@ function load_functions() {
         fi
 
         # Epilogue
-        add_entry LOADED_PATHS "`echo "(${loaded_paths[@]})"`"
+        add_entry LOADED_PATHS "$(echo "(${loaded_paths[@]})")"
 
         # Print the file to the console
         printf "Generated paths.sh file:\n\n"
@@ -116,18 +127,18 @@ function load_functions() {
             fi
         done
         if [ "$validation_error" == 1 ]; then
-            cache_file_path=`realpath "$BASH_SOURCE" | xargs dirname`.paths.sh
+            cache_file_path=$(realpath "${BASH_SOURCE[0]}" | xargs dirname).paths.sh
             echo "Invalid path(s) came from \"$cache_file_path\" cache file."
             echo "Deleting the file will cause regeneration at next terminal launch."
         fi
     fi
 
     # Load all functions to namespace
-    while read script_file; do
+    while read -r script_file; do
         . "$script_file"
-    done <<< `find . -mindepth 2 | grep "\.sh$" | grep -v "^\./Runnable/"`
+    done <<< $(find . -mindepth 2 | grep "\.sh$" | grep -v "^\./Runnable/")
 
-    popd > /dev/null
+    cd "$initial_dir" || return
 }
 
-load_functions $@
+load_functions "$@"
