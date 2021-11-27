46a6c9c6f67e84a415ace2a6e2d27c30704a3779
MaciejDziuban <dziuban.maciej@gmail.com>
2021-11-27 14:30:06 +0100

    Add scc script

--- BashUtils/Misc/sc.sh
+++ BashUtils/Misc/sc.sh
@@ -7,3 +7,10 @@ fi
 sc() (
     shellcheck --exclude=SC2155,SC1090,SC2044 "$@" 
 )
+
+scc() (
+    find $SCRIPTS_PATH $LINUX_SETUP_ROOT -type f                                              \
+        \( -name "*.sh" -o -name "*.bash" \)                                                  \
+        -not -path "$LINUX_SETUP_ROOT/build/*" -not -path "$SCRIPTS_PATH/load_functions.bash" \
+        |  xargs -l1 shellcheck -e 2086,2181,2038,2009,2068,2046,2155
+)
