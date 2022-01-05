6c8bc06ac4310ca92585b73396a15ace96f73807
MaciejDziuban <dziuban.maciej@gmail.com>
2022-01-05 23:57:42 +0100

    Fix p script on windows

--- BashUtils/Core/oneletter/p.sh
+++ BashUtils/Core/oneletter/p.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-alias p="ps aux --forest "
-alias pp="ps aux --forest | less"
+if [ "$IS_LINUX" = 0 ]; then
+    alias p="ps aux --forest"
+else
+    alias p="ps -W"
+fi
+
+alias pp="p | less"
