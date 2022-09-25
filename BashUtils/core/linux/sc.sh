a1633ac35281ce034435d47a955e519ba6183223
MaciejDziuban <dziuban.maciej@gmail.com>
2022-09-25 23:27:24 +0200

    Do not print errors if some dotfiles are not present

--- BashUtils/core/linux/sc.sh
+++ BashUtils/core/linux/sc.sh
@@ -11,7 +11,7 @@ scc() (
           -not -path "$LINUX_SETUP_ROOT/build/*" -not -path "$SCRIPTS_PATH/load_functions.bash"
     }
     seek_for_dotfiles_scripts() {
-        find ~/.profile ~/.bashrc ~/.bash_profile ~/.xinitrc
+        find ~/.profile ~/.bashrc ~/.bash_profile ~/.xinitrc 2>/dev/null
     }
 
     (seek_for_regular_scripts ; seek_for_dotfiles_scripts) |  xargs -l1 shellcheck -e 2086,2181,2038,2009,2068,2046,2155,2044,1090,1091,2059,2015
