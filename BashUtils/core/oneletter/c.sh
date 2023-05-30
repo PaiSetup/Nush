a52466ab0bbb34562f41ad71f3c3567076b75fa7
MaciejDziuban <dziuban.maciej@gmail.com>
2023-05-30 22:05:01 +0200

    Improve "c" script
    1. Add an option to cd into the cloned repo without opening a new terminal
    2. Transform URL from https to ssh

--- BashUtils/core/oneletter/c.sh
+++ BashUtils/core/oneletter/c.sh
@@ -3,7 +3,7 @@
 # c stands for clone. It clones git respositories easily to the work dir and opens it with an editor. Any Github link will work
 # Example:
 #    c https://github.com/InternalMD/Scripts/blob/master/BashUtils/load_functions.sh
-c() (
+c() {
     url="$1"
 
     # Find root dir for cloning repositories
@@ -12,8 +12,9 @@ c() (
         project_dir="$PROJECT_DIR"
     fi
 
-    # Get base url
-    url="$(echo "$1" | grep -oE "https?://github\.com/[^/]+/[^/]+")"
+    # Preprocess url
+    url="$(echo "$url" | grep -oE "https?://github\.com/[^/]+/[^/]+")" # Get base url - without directories, commit etc.
+    url="$(echo "$url" | sed -E "s/https:\/\/github\.com\/(.*)/git@github.com:\1/g")" # Change to ssh
     if [ $? != 0 ]; then
         echo "ERROR: invalid url" >&2
         return 1
@@ -37,13 +38,15 @@ c() (
     echo "  1. VsCode"
     echo "  2. \$FILE_MANAGER ($FILE_MANAGER)"
     echo "  3. \$TERMINAL ($TERMINAL)"
-    echo "  4. No"
+    echo "  4. cd into it"
+    echo "  5. No"
     printf "Selection [1]: "
     read -r selection
     case "$selection" in
         2) $FILE_MANAGER "$repo_dir" & ;;
         3) $TERMINAL sh -c "cd $repo_dir; bash" & ;;
-        4) ;;
+        4) cd $repo_dir || return 1 ;;
+        5) ;;
         *) code "$repo_dir" & ;;
     esac
-)
+}
