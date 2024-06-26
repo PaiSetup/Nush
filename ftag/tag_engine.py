f41630a51fe149c779733e1a40b8e8dca5aabe54
MaciejDziuban <dziuban.maciej@gmail.com>
2024-06-26 17:52:37 +0200

    Ftag: refactor - separate metadata operations from logic

--- ftag/tag_engine.py
+++ ftag/tag_engine.py
@@ -1,11 +1,12 @@
 import enum
-import hashlib
 import json
 import os
 import random
 import re
 import shutil
 from pathlib import Path
+
+from utils import get_file_hash
 
 tagged_directory_name = "ftags"
 metadata_file_name = "ftags.json"
@@ -26,6 +27,97 @@ class TagEngineException(Exception):
             self.message += " This is a developer error. It should never happen and it's likely a bug in ftag."
 
 
+class TagEngineMetadata:
+    def __init__(self, metadata_file_path):
+        if metadata_file_path is not None:
+            self.load(metadata_file_path)
+        else:
+            self.load_empty()
+
+    def load_empty(self):
+        self._metadata = {
+            "files": {},
+            "tags": {},
+        }
+
+    def load(self, metadata_file_path):
+        with open(metadata_file_path, "r") as file:
+            self._metadata = json.load(file)
+
+        # Simple basic validation
+        if "files" not in self._metadata:
+            return False
+        if "tags" not in self._metadata:
+            return False
+
+    def save(self, metadata_file_path, tmp_file):
+        with open(tmp_file, "w") as file:
+            content = json.dump(self._metadata, file, indent=4)
+        shutil.move(tmp_file, metadata_file_path)
+
+    def is_untagged(self, file_path, categories):
+        file_hash = get_file_hash(file_path)
+        if file_hash not in self._metadata["files"]:
+            return True
+
+        file_tags = list(self._metadata["files"][file_hash]["tags"].keys())
+        return any((c not in file_tags for c in categories))
+
+    def get_tag_categories(self):
+        return list(self._metadata["tags"].keys())
+
+    def get_tag_values(self, category):
+        return list(self._metadata["tags"][category])
+
+    def get_file_tags(self, file_path):
+        file_hash = get_file_hash(file_path)
+        if file_hash is None:
+            raise TagEngineException(f"File {file_path} does not exist")
+
+        if file_hash not in self._metadata["files"]:
+            raise TagEngineException(f"File {file_path} is not tagged")
+
+        return self._metadata["files"][file_hash]["tags"]
+
+    def get_tags_for_file(self, file_path, category):
+        tags = self.get_file_tags(file_path)
+        if category not in tags:
+            raise TagEngineException(f"File {file_path} is not tagged for category {category}")
+        return tags[category]
+
+    def add_category(self, category_name):
+        if not re.match(name_regex, category_name):
+            raise TagEngineException(f'Category name "{category_name}" is not allowed.')
+        if category_name in self._metadata["tags"]:
+            raise TagEngineException(f'Category name "{category_name}" already exists.')
+        self._metadata["tags"][category_name] = []
+
+    def add_tag(self, category, new_tag):
+        if not re.match(name_regex, new_tag):
+            raise TagEngineException(f'Tag name "{new_tag}" is not allowed.')
+        if category not in self._metadata["tags"]:
+            raise TagEngineException(f'Unknown category "{category}"', developer_error=True)
+        if new_tag in self._metadata["tags"][category]:
+            raise TagEngineException(f'Tag "{new_tag}" already exists')
+        self._metadata["tags"][category].append(new_tag)
+
+    def set_tags(self, file_path, tags):
+        file_hash = get_file_hash(file_path)
+        if file_hash is None:
+            raise TagEngineException(f"File {file_path} does not exist")
+
+        # Create new entry, if file is not in the database. Only tags can be changed. Rest of the metadata
+        # is constant. Hash is unique identifier. Path is only for sanity checks, but it's not used.
+        if file_hash not in self._metadata["files"]:
+            self._metadata["files"][file_hash] = {
+                "path": str(file.absolute().relative_to(self._get_root_dir_path())),
+                "tags": {},
+            }
+
+        # Entry must be created by now. Set the tags
+        self._metadata["files"][file_hash]["tags"] = dict(tags)
+
+
 class TagEngine:
     def __init__(self):
         self._root_dir = None
@@ -33,7 +125,7 @@ class TagEngine:
 
         self._root_dir = TagEngine._find_root_dir()
         if self._root_dir is not None:
-            self._metadata = TagEngine._read_metadata(self.get_metadata_file())
+            self._metadata = TagEngineMetadata(self.get_metadata_file())
 
             if self._metadata is None:
                 self._state = TagEngineState.InvalidData
@@ -44,10 +136,7 @@ class TagEngine:
 
     def initialize(self):
         self._root_dir = Path(os.path.abspath(os.curdir))
-        self._metadata = {
-            "files": {},
-            "tags": {},
-        }
+        self._metadata = TagEngineMetadata(None)
         self.save()
         self._state = TagEngineState.Loaded
 
@@ -64,19 +153,6 @@ class TagEngine:
             current_path = current_path.parent
         return None
 
-    @staticmethod
-    def _read_metadata(file_path):
-        with open(file_path, "r") as file:
-            metadata = json.load(file)
-
-        # Simple basic validation
-        if "files" not in metadata:
-            return False
-        if "tags" not in metadata:
-            return False
-
-        return metadata
-
     def _get_root_dir_path(self):
         return self._root_dir
 
@@ -89,14 +165,16 @@ class TagEngine:
     def get_metadata_file(self):
         return self._root_dir / metadata_file_name
 
+    def get_tag_categories(self):
+        return self._metadata.get_tag_categories()
+
+    def get_tag_values(self, category):
+        return self._metadata.get_tag_values(category)
+
     def save(self):
         real_file = self.get_metadata_file()
         tmp_file = self._get_root_dir_path() / metadata_file_name_tmp
-
-        with open(tmp_file, "w") as file:
-            self._root_dir / metadata_file_name
-            content = json.dump(self._metadata, file, indent=4)
-        shutil.move(tmp_file, real_file)
+        self._metadata.save(real_file, tmp_file)
 
     def _get_taggable_files(self):
         for root, dirs, files in os.walk(self._get_root_dir_path()):
@@ -109,20 +187,12 @@ class TagEngine:
                 yield file_path
 
     def get_untagged_files(self, randomize=True):
-        def is_untagged(file_path, categories):
-            file_hash = TagEngine._get_file_hash(file_path)
-            if file_hash not in self._metadata["files"]:
-                return True
-
-            file_tags = list(self._metadata["files"][file_hash]["tags"].keys())
-            return any((c not in file_tags for c in categories))
-
-        categories = self.get_tag_categories()
+        categories = self._metadata.get_tag_categories()
         files = self._get_taggable_files()
         if randomize:
             files = list(files)
             random.shuffle(files)
-        return (f for f in files if is_untagged(f, categories))
+        return (f for f in files if self._metadata.is_untagged(f, categories))
 
     def generate_all_files(self, cleanup):
         if cleanup:
@@ -152,83 +222,27 @@ class TagEngine:
             print(f"File {file_path} does not exist")
             return
 
-        # Early return if this file is not tagged.
-        if file_hash not in self._metadata["files"]:
+        # Get tags of the file as a dictionary - key is category name, value is list of tags
+        try:
+            file_tags = self._metadata.get_file_tags(file_path)
+        except TagEngineException as e:
+            print(e.message)
             return
 
         # Iterate over all tags assigned to this file and generate symlinks for each one.
-        file_tags = self._metadata["files"][file_hash]["tags"]
         for category, values in file_tags.items():
             for value in values:
                 symlink_dir = f"{self._get_symlink_root()}/{category}/{value}"
                 symlink(file_path, symlink_dir, file_hash)
 
-    def dump_raw_metadata_to_console(self):
-        print(json.dumps(self._metadata, indent=4))
-
-    def get_tag_categories(self):
-        return list(self._metadata["tags"].keys())
-
-    def get_tag_values(self, category):
-        return list(self._metadata["tags"][category])
-
     def add_category(self, category_name):
-        if not re.match(name_regex, category_name):
-            raise TagEngineException(f'Category name "{category_name}" is not allowed.')
-        if category_name in self.get_tag_categories():
-            raise TagEngineException(f'Category name "{category_name}" already exists.')
-        self._metadata["tags"][category_name] = []
+        self._metadata.add_category(category_name)
 
     def add_tag(self, category, new_tag):
-        if not re.match(name_regex, new_tag):
-            raise TagEngineException(f'Tag name "{new_tag}" is not allowed.')
-        if category not in self._metadata["tags"]:
-            raise TagEngineException(f'Unknown category "{category}"', developer_error=True)
-        if new_tag in self._metadata["tags"][category]:
-            raise TagEngineException(f'Tag "{new_tag}" already exists')
-        self._metadata["tags"][category].append(new_tag)
+        self._metadata.add_tag(category_name)
 
-    def get_tags_for_file(self, file, category):
-        file_hash = TagEngine._get_file_hash(file)
-        if file_hash is None:
-            return None
-        if file_hash not in self._metadata["files"]:
-            return None
-        categories = self._metadata["files"][file_hash]["tags"]
-        if category not in categories:
-            return None
-        return categories[category]
+    def get_tags_for_file(self, file_path, category):
+        return self._metadata.get_tags_for_file(file_path, category)
 
-    def set_tags(self, file, tags):
-        file_hash = TagEngine._get_file_hash(file)
-        if file_hash is None:
-            raise Exception("Wrong file in set_tags")  # TODO
-
-        # Create new entry, if file is not in the database. Only tags can be changed. Rest is of metadata
-        # is constant. Hash is unique identifier. Path is only for sanity checks, but it's not used.
-        if file_hash not in self._metadata["files"]:
-            self._metadata["files"][file_hash] = {
-                "path": str(file.absolute().relative_to(self._get_root_dir_path())),
-                "tags": {},
-            }
-
-        # Entry must be created by now. Set the tags
-        self._metadata["files"][file_hash]["tags"] = dict(tags)
-
-    @staticmethod
-    def _get_file_hash(file_path):
-        try:
-            file_size = os.path.getsize(file_path)
-        except FileNotFoundError:
-            return None
-        bytes_left = min(128 * 1024, file_size)
-        chunk_size = 8096
-
-        hash_function = hashlib.new("blake2b")
-        with open(file_path, "rb") as file:
-            while bytes_left != 0:
-                current_chunk_size = min(chunk_size, bytes_left)
-                chunk = file.read(current_chunk_size)
-                hash_function.update(chunk)
-                bytes_left -= current_chunk_size
-        return hash_function.hexdigest()[0:48]
+    def set_tags(self, file_path, tags):
+        self._metadata.set_tags(file_path, tags)
--- ftag/utils.py
+++ ftag/utils.py
@@ -1,5 +1,25 @@
+import hashlib
+import os
 import re
 import sys
+
+
+def get_file_hash(file_path):
+    try:
+        file_size = os.path.getsize(file_path)
+    except FileNotFoundError:
+        return None
+    bytes_left = min(128 * 1024, file_size)
+    chunk_size = 8096
+
+    hash_function = hashlib.new("blake2b")
+    with open(file_path, "rb") as file:
+        while bytes_left != 0:
+            current_chunk_size = min(chunk_size, bytes_left)
+            chunk = file.read(current_chunk_size)
+            hash_function.update(chunk)
+            bytes_left -= current_chunk_size
+    return hash_function.hexdigest()[0:48]
 
 
 class CliException(Exception):
